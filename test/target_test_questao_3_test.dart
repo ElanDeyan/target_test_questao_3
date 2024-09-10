59081b867282531666d3dccd553357b40da3de59
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-09-09 21:50:43 -0300

    test: adds test

--- test/target_test_questao_3_test.dart
+++ test/target_test_questao_3_test.dart
@@ -1,16 +1,34 @@
+import 'dart:math';
+
 import 'package:target_test_questao_3/target_test_questao_3.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
+  test('Simple test', () {
+    final faturamento = [
+      for (var i = 0; i < 50; i++) Random().nextDouble() * 500
+    ];
 
-    setUp(() {
-      // Additional setup goes here.
-    });
+    final faturamentosPostivivosOrdenado = faturamento
+        .where(
+          (element) => element > 0.0,
+        )
+        .toList()
+      ..sort();
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
+    final menorFaturamento = faturamentosPostivivosOrdenado.first;
+    final maiorFaturamento = faturamentosPostivivosOrdenado.last;
+    final media =
+        faturamentosPostivivosOrdenado.fold(0.0, (prev, elem) => prev + elem) /
+            faturamentosPostivivosOrdenado.length;
+
+    expect(
+        getInsightsFaturamento(faturamento).menorFaturamento, menorFaturamento);
+    expect(
+        getInsightsFaturamento(faturamento).maiorfaturamento, maiorFaturamento);
+    expect(
+        faturamentosPostivivosOrdenado
+            .where((faturamento) => faturamento > media),
+        hasLength(getInsightsFaturamento(faturamento).diasAcimaDaMedia));
   });
 }
