3ce3cd63b04687920317758315e47664ddcfcab5
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-09-09 21:28:46 -0300

    build: first commit

---
+++ example/target_test_questao_3_example.dart
@@ -0,0 +1,6 @@
+import 'package:target_test_questao_3/target_test_questao_3.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/target_test_questao_3_test.dart
@@ -0,0 +1,16 @@
+import 'package:target_test_questao_3/target_test_questao_3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
