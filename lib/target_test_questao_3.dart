2445f3f6ed67ab378884c2e2b51ca468543c7f9a
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-09-09 21:50:23 -0300

    feat: adds function to get insights

--- lib/target_test_questao_3.dart
+++ lib/target_test_questao_3.dart
@@ -3,6 +3,27 @@
 /// More dartdocs go here.
 library;
 
-export 'src/target_test_questao_3_base.dart';
+typedef FaturamentoInsights = ({
+  double menorFaturamento,
+  double maiorfaturamento,
+  int diasAcimaDaMedia,
+});
 
-// TODO: Export any libraries intended for clients of this package.
+/// Solucao para a questao 3 do desafio desenvolvedor da target
+FaturamentoInsights getInsightsFaturamento(
+    List<double> faturamentoDiarioAnual) {
+  final diasComFaturamento = faturamentoDiarioAnual
+      .where((faturamento) => faturamento > 0.0)
+      .toList()
+    ..sort();
+
+  final media = diasComFaturamento.fold(0.0, (prev, elem) => prev + elem) /
+      diasComFaturamento.length;
+
+  return (
+    menorFaturamento: diasComFaturamento.first,
+    maiorfaturamento: diasComFaturamento.last,
+    diasAcimaDaMedia:
+        diasComFaturamento.where((faturamento) => faturamento > media).length,
+  );
+}
